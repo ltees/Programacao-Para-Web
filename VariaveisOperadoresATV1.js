0d6213abf53d72ead6c214df451c5c91596de648
Lucas Teles <135617476+ltees@users.noreply.github.com>
2025-02-26 09:35:06 -0300

    Add files via upload

---
+++ VariaveisOperadoresATV1.js
@@ -0,0 +1,10 @@
+let num1 = 20, num2 = 15;
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log(`Resultado da soma entre ${num1} e ${num2} é ${soma}`);
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+console.log(`Resultado da soma multiplicação ${num1} e ${num2} é ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${divisao}`);
