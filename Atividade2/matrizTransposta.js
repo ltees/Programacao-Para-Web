e3c85ba817e9a2bbead62656c9dcdc85728ddd00
Lucas Teles <135617476+ltees@users.noreply.github.com>
2025-03-18 10:04:59 -0300

    matrizTransposta.js

--- Atividade2/matrizTransposta.js
+++ Atividade2/matrizTransposta.js
@@ -8,6 +8,13 @@ function transporMatriz(A) {
     }
 
     console.log("\nTransposta\n");
+    for(let j = 0; j < A[0].length; j++){
+        let linha = "";
+        for(let i = 0; i < A.length; i++){
+            linha += A[i][j] + "\t";
+        }
+        console.log(linha);
+    }
 }
 
 let matriz = [
