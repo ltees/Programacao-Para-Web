d6dbe07c45e4085a8a5bae0ae7217644d35f3edf
Lucas Oliveira Teles <135617476+ltees@users.noreply.github.com>
2025-06-10 09:26:03 -0300

    agendamentoController.js

--- Atividade5/controllers/agendamentoController.js
+++ Atividade5/controllers/agendamentoController.js
@@ -1,12 +1,39 @@
+const AgendamentoConsulta = require('../models/agendamentoConsultaModel');
+
 function getIndexView(req, res){
     res.render('index.html');
 }
 
-function postAgendarConsulta(req, res){
-    let form_invalido = false;
-    let campos_invalidos = [];
+function getAgendamentosView(req, res){
+    AgendamentoConsulta.findAll().then((agendamentos)=>{
+        res.render('agendamentos.html', {agendamentos});
+    });
+}
 
+function postAgendarConsulta(req, res){
     let dados_consulta = req.body;
+    let campos_invalidos = validarRequisicaoAgendamentoConsulta(dados_consulta);
+
+    if(campos_invalidos.length == 0){
+        AgendamentoConsulta.create(dados_consulta).then(()=>{
+            res.redirect('/agendamentos');
+        });
+    }
+    else{
+        res.render('index.html', {campos_invalidos, dados_consulta});
+    }
+
+    
+}
+
+module.exports = {
+    getIndexView,
+    postAgendarConsulta,
+    getAgendamentosView
+}
+
+function validarRequisicaoAgendamentoConsulta(dados_consulta){
+    let campos_invalidos = [];
 
     if(dados_consulta.nome.length == 0){
         form_invalido = true;
@@ -21,7 +48,7 @@ function postAgendarConsulta(req, res){
         campos_invalidos.push("CPF");
     }
 
-    res.render('index.html', {form_invalido, campos_invalidos, dados_consulta});
+    return campos_invalidos;
 }
 
 module.exports = {
